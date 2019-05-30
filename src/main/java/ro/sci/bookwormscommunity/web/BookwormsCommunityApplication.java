471120bdfd21ff71aafa380da04314fc330d66bf
AlexNovacean <novacean.alex@gmail.com>
2019-05-30 20:48:39 +0300

    Created Project Structure

---
+++ src/main/java/ro/sci/bookwormscommunity/web/BookwormsCommunityApplication.java
@@ -0,0 +1,13 @@
+package ro.sci.bookwormscommunity.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwormsCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwormsCommunityApplication.class, args);
+	}
+
+}
